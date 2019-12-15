fc62c65bcad1e6c1762c6c2160cea400b7a65dec
Praveen Saraogi <saraogipraveen@gmail.com>
2019-12-15 15:23:35 +0530

    focus issue fixed

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,40 +2,30 @@ import React, { useState, useEffect } from 'react';
 import Container from '../styled_components/Container';
 import styled from 'styled-components';
 
-const Weather = (props) => {
-
-    const SearchBox = styled.div`
-    display : flex;
-    flex-direction: row;
-    `
-
-    const InputBox = styled.input`
-    border-radius : 5px;
-    border : 3px solid darksalmon;
+const SearchBox = styled.div`
+display : flex;
+flex-direction: row;
 `;
 
-    const [searchText, setSearchText] = useState('default')
+const InputBox = styled.input`
+border-radius : 5px;
+border : 3px solid darksalmon;
+`;
 
-    useEffect((props) => {
-        console.log("TCL: Weather -> props", props)
-    }, [])
-
+const Weather = (props) => {
+    const [searchText, setSearchText] = useState("")
     const getWeather = () => {
         console.log("TCL: getWeather -> getWeather", searchText)
     }
-
-    console.log("TCL: Weather -> searchText", searchText)
     return (
         <Container>
             <h1>Current Weather</h1>
             <SearchBox>
-                <InputBox type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)}></InputBox>
+                <InputBox name={'search'} type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)}></InputBox>
                 <button onClick={getWeather}>Search</button>
             </SearchBox>
         </Container>
     )
-
 }
-
 
 export default Weather;
