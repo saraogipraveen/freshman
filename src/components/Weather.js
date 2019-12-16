f2533a79b1ead272d8de3b28ab7783a779f371db
Praveen Saraogi <saraogipraveen@gmail.com>
2019-12-16 23:17:28 +0530

    API call , container css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,28 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import Container from '../styled_components/Container';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const SearchBox = styled.div`
 display : flex;
-flex-direction: row;
+justify-content: center;
 `;
 
 const InputBox = styled.input`
+outline : none;
+margin-right : 20px;
 border-radius : 5px;
-border : 3px solid darksalmon;
+border : 3px solid orangered;
+
+&:focus{
+    box-shadow: 0px 2px 2px 2px lightskyblue;
+}
 `;
+
+const Button = styled.button`
+`
+
+
+const API_KEY = 'd1d5d8b72a9521525bb28a6b5d75bba1'
+const country = 'ind'
 
 const Weather = (props) => {
     const [searchText, setSearchText] = useState("")
-    const getWeather = () => {
-        console.log("TCL: getWeather -> getWeather", searchText)
+
+    const getWeather = async() => {
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText},${country}&appid=${API_KEY}`
+            const response = await axios.get(url);
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
     }
+
+    const searchClicked = () => {
+        getWeather();
+    }
+
     return (
         <Container>
             <h1>Current Weather</h1>
             <SearchBox>
                 <InputBox name={'search'} type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)}></InputBox>
-                <button onClick={getWeather}>Search</button>
+                <Button onClick={searchClicked}>Search</Button>
             </SearchBox>
         </Container>
     )
--- src/styled_components/Container.js
+++ src/styled_components/Container.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
-export default  styled.div`
-display : flex;
-flex-direction:column;
-height: 400px;
-justify-content: center;
-align-items: center;
+export default styled.div`
+position:absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+position: absolute;
+width: 100%;
 font-size : 16px;
+text-align: center;
 `;
